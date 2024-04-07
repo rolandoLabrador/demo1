058fc52c7dbd060798865650f5cdc77eff0825f5
Roland Labrador <rolando1515@hotmail.com>
2024-04-07 12:46:07 -0400

    Commit

---
+++ src/test/java/demo/demo1/SeleniumDemo01.java
@@ -0,0 +1,20 @@
+package demo.demo1;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SeleniumDemo01 {
+
+     @Test
+     void setup(){
+
+
+
+     }
+
+}
